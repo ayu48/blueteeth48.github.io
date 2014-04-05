273cb2a3db4501cb94f0a5617ae0214b0379baae
blueteeth48 <ayuishii.blue+git@gmail.com>
2014-04-05 16:06:48 +0900

    use vegas slideshow for top bg

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(window).load(function() {
     $(".navbar").sticky({ topSpacing: 0});
+    $.vegas('slideshow', {
+        backgrounds: [
+            {src: 'img/fishing.jpg', fade: 2000},
+            {src: 'img/clouds.jpg', fade: 2000},
+            {src: 'img/wedding.jpg', fade: 2000}
+        ]
+    })('overlay', {
+        src: 'img/overlay.png'
+    });
 })
