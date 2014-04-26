363c49a497a14254a971a83a2c5cb24eb8558ba1
blueteeth48 <ayuishii.blue+git@gmail.com>
2014-04-26 14:37:37 +0900

    renew website

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(window).load(function() {
+    $.vegas('slideshow', {
+        backgrounds: [
+            {src: 'img/fishing.jpg', fade: 2000},
+            {src: 'img/clouds.jpg', fade: 2000},
+            {src: 'img/wedding.jpg', fade: 2000}
+        ]
+    });
+    instagram.show();
+})
+
+//FIXME!!!
+var instagram = {
+    show: function() {
+        var url = 'https://api.instagram.com/v1/users/2157886/media/recent/?client_id=816523ede91545e68dcfd0b27fd8e09f';
+        var images = [];
+        $.ajax({
+            type: 'GET',
+            url: url,
+            async: false,
+            jsonpCallback: 'jsonCallback',
+            contentType: "application/json",
+            dataType: 'jsonp',
+            data: {
+                'count': 10
+            },
+            success: function(json) {
+                images = $.map(json['data'], function(item) {
+                    return item['images']['standard_resolution']['url'];
+                });
+                $('#instagram-photos div.item').map(function(k, dom) {
+                    $('a', dom).attr('href', images[k]);
+                    $('img', dom).attr('src', images[k]);
+                });
+            },
+            error: function(e) {
+                console.log(e.message);
+            }
+        });
+    }
+}
--- js/top.js
+++
@@ -1,9 +0,0 @@
-$(window).load(function() {
-    $.vegas('slideshow', {
-        backgrounds: [
-            {src: 'img/fishing.jpg', fade: 2000},
-            {src: 'img/clouds.jpg', fade: 2000},
-            {src: 'img/wedding.jpg', fade: 2000}
-        ]
-    });
-})
